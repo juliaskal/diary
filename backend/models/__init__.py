a59bc070e7bf5d7c3fa7e0747ac78f2ad369f81e
Юлия Скалецкая <y-masya1@mail.ru>
2026-04-21 22:06:42 +0300

    ✨ (models): Creat emotion model

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,6 +3,8 @@ from models.freeze_model import FreezeModel
 from models.domain.post import Post
 from models.domain.folder import Folder
 from models.post_request import PostRequest
+from models.domain import Emotion
+
 
 __all__ = [
     'DBMS',
@@ -10,4 +12,5 @@ __all__ = [
     'Post',
     'Folder',
     'PostRequest',
+    'Emotion',
 ]
--- backend/models/domain/__init__.py
+++ backend/models/domain/__init__.py
@@ -1,8 +1,10 @@
 from models.domain.post import Post
 from models.domain.folder import Folder
+from models.domain.emotion import Emotion
 
 
 __all__ = [
     "Post",
     "Folder",
+    "Emotion",
 ]

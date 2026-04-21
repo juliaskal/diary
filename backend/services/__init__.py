0149a272091e4a9089b1ae8c69da35e249b80109
Юлия Скалецкая <y-masya1@mail.ru>
2026-04-21 22:09:22 +0300

    ✨(services): Create EmotionService

---
+++ backend/services/__init__.py
@@ -0,0 +1,10 @@
+from services.folder_service import FolderService
+from services.emotion_service import EmotionService
+from services.post_service import PostService
+
+
+__all__ = [
+    "FolderService",
+    "EmotionService",
+    "PostService",
+]
